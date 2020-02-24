25bee226eec0e66fe3b633fca3b58594fd408f12
Juan Mendoza <juan.mendoza@deliveryhero.com>
2020-02-24 22:49:22 +0100

    Using JSON-API with json-resources GEM

---
+++ notebook-api-json/peeps/config/application.rb
@@ -0,0 +1,20 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+# Example only, please understand CORS before blindly adding this configuration
+# This is not enabled in the peeps source code.
+module Peeps
+  class Application < Rails::Application
+    config.middleware.insert_before 0, Rack::Cors, :debug => !Rails.env.production?, :logger => (-> { Rails.logger }) do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :patch, :delete, :options]
+      end
+    end
+  end
+end
